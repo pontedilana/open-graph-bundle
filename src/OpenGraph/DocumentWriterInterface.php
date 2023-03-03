85e0de8d0a37ec3bd77a7d92d327dae44fca0db2
Manuel Dalla Lana <manuel@pontedilana.it>
2023-03-03 18:59:22 +0100

    Declare DocumentWriterInterface public methods

--- src/OpenGraph/DocumentWriterInterface.php
+++ src/OpenGraph/DocumentWriterInterface.php
@@ -7,9 +7,13 @@ namespace Pontedilana\OpenGraphBundle\OpenGraph;
  *
  * @package Pontedilana\OpenGraphBundle\OpenGraph
  * @author  Nikita Loges
- * 
+ * @author  Manuel Dalla Lana
  */
 interface DocumentWriterInterface
 {
+    public function append(string $property, string $content): \Opengraph\Opengraph;
 
+    public function prepend(string $property, string $content):\Opengraph\Opengraph;
+
+    public function render(string $indent): string;
 }
